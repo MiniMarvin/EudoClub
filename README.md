8c029f5947b8da2f5a362f98ad4bd070baf0f8ef
Ramon Wanderley <ramonwanderley13@gmail.com>
2017-08-12 17:56:55 +0000

    Adicionando Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# EudoraClub
+
+O projeto EudoraClub tem a finalidade de formetar, desenvolver e melhorar a comunidade de revendedores da Eudora
+O EudoraClub é um sistema de gameficação de atividades para as revendedoras eudora, onde é possível ganhar pontos para trocar por descontos na compra de produtos à empresa e investir mais em seu negócio, além de ser possível fazer uso deles para investir em causas sociais, como as que já são apoiadas pelo Grupo Boticário.
+
+## Funcionamento
+
+O EudoraClub funciona sobre um WebApp construído usando um modelo de chamadas de API. Também foram utilizadas as tecnologias HTML5, CCS3, JavaScript, JQuery, PHP e MySQL, para o desenvolvimento do projeto.
+
--- back/db.sql
+++ back/db.sql
@@ -37,14 +37,73 @@ INSERT INTO revendedoras (nome, categoria, pontos, causas)
 VALUES ("Eunice", "begin", 190, "Plantação de árvores da Eudora");
 
 INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
-VALUES ("Sheila", "begin", 240, "Plantação de árvores da Eudora", "Recife");
+VALUES ("Sheila", "begin", 2400, "Plantação de árvores da Eudora", "Recife");
 
 INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
-VALUES ("Bruna", "grower", 2400, "Plantação de árvores da Eudora", "Recife");
+VALUES ("Bruna", "grower", 2400, "batom kissme", "Recife");
 
 INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
-VALUES ("Eunuco", "expert", 20400, "Plantação de árvores da Eudora", "Recife");
+VALUES ("Eunuco", "expert", 2600, "Eudora shower gel ", "Recife");
 
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Yaucha", "expert", 2900, "Plantação de árvores Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Maely", "begin", 1001, "Base Eudora soul matte", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Rayane", "grower", 2200, "Pó facial multiefeito Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Marcos", "begin", 1135, "Plantação de árvores da Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Pedro", "grower", 2300, "Spray corporal perfumado força", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Carolina", "begin", 1400, "Brilho labial soul get flash", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Valdinete", "grower", 2333, "Sabonete líquido corporal aflorá", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Jéssica", "expert", 3007, "Plantação de árvores da Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Laura", "expert", 3000, "Sombra retrátil para olhos Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Estela", "begin", 1900, "Soul duplo impacto", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Luana", "grower", 2240, "Soul caminhe na areia", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Fátima", "grower", 2144, "Plantação de árvores Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Vladimir", "expert", 2999, "On Men Night", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Júlia", "begin", 1870, "Magnetic Eyes", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Myrella", "begin", 1755, "Shampoo Siàge", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Isabelle", "expert", 2898, "Soul duplo impacto", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Gabriela", "grower", 2549, "Plantação de árvores Eudora", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("João", "grower", 2499, "Desodorante Carbon", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Luzinete", "grower", 2347, "Gel creme clareador antissinais", "Recife");
+
+INSERT INTO revendedoras (nome, categoria, pontos, causas, cidade)
+VALUES ("Genival", "begin", 1888, "Balm pós-barba", "Recife");
 /* Create New user in new table */
 INSERT INTO revendedoras (nome, categoria, cidade, pontos, causas, bruto_ant, bruto_best, bruto_total, bruto, bruto_ant, bruto_best, bruto_total, bruto)
 VALUES ("Eunice", "", 190, "Plantação de árvores da Eudora");
